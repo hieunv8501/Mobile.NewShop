b5d5c19dabc0e56d3a608d96185723c8c1d1e5a1
Nguyễn Văn Hiếu <67740526+hieunv8501@users.noreply.github.com>
2021-10-25 17:50:43 +0700

    Initial project
    This commit for initializing project

---
+++ DoAn/DoAn/DoAn/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoAn
+{
+    public partial class App : Application
+    {
+        public static string TenDangNhapSignedIn = null;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DoAn/DoAn/DoAn/TaiKhoan.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoAn
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaiKhoan : ContentPage
+    {
+        public TaiKhoan()
+        {         
+            InitializeComponent();
+        }
+
+        private void btnDNDK_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DangNhap());
+        }
+    }
+}
