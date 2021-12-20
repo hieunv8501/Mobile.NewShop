2a26e9b58866ae550ae486bf52ac312964b1ad5d
= <=>
2021-12-20 12:51:17 +0700

    GioHang(Chua xong :))

---
+++ DoAn/DoAn/DoAn/GioHangTheoTK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoAn
+{
+    public class GioHangTheoTK
+    {
+        public string TenSach { get; set; }
+        public double ThanhTien { get; set; }
+        public int SoLuong { get; set; }
+        public string Hinh { get; set; }
+        public double TongTien { get; set; }
+        public int MaGioHang { get; set; }
+        public int MaSach { get; set; }
+    }
+}
